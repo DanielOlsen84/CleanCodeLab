35c9b2a82ff3232c79bd374374847dd96888ce79
Daniel Olsen <daniel_olsen84@hotmail.com>
2022-12-19 13:43:54 +0100

    cut out more unused usings

--- Enums/GameTitleEnum.cs
+++ Enums/GameTitleEnum.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CleanCodeLab.Enums
+﻿namespace CleanCodeLab.Enums
 {
     public class GameNameEnum
     {
